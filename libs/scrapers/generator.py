6e830bf696704890ec9e34b224c90d6a6f40df78
aizenkhan <altamashjahangirkhan123@gmail.com>
2021-07-28 00:22:05 +0530

    Added names generator

---
+++ libs/scrapers/generator.py
@@ -0,0 +1,50 @@
+"""Base script to generate all pokemon names for a given generation"""
+
+
+class InvalidGenError(Exception):
+    pass
+
+
+class PokemonNamesGenerator:
+    URL = "https://www.serebii.net/pokemon/gen{gen_num}pokemon.shtml"
+    VALID_GENS = ["1"]
+
+    def __init__(self, gen, out_path):
+        self.gen = gen
+        self.out_path = out_path
+        self.validate()
+
+    def validate(self):
+        if self.gen is None or self.gen not in self.VALID_GENS:
+            raise InvalidGenError(f"Invalid Pokemon Generation: {self.gen}")
+        LOG.info("Successfully validated params: %s", self.__dict__)
+
+    def parse(self):
+        url = self.URL.format(gen_num=gen)
+        pokedex = pd.read_html(url, header=1)[0]
+        pokemon_names = pokedex.loc[:, "Name"]
+        LOG.info("Parsed table from url: %s", url)
+        pokemon_names.to_csv(self.out_path, index=False)
+        LOG.info("Successfully written names to: %s", self.out_path)
+
+
+if __name__ == "__main__":
+    import pandas as pd
+    import argparse
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+
+    LOG = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-g", "--gen", type=str, required=True, help="the generation #."
+    )
+
+    parser.add_argument("-o", "--output", type=str, required=True, help="output file.")
+    args = parser.parse_args()
+    gen = args.gen
+    out = args.output
+    nameGenerator = PokemonNamesGenerator(gen=gen, out_path=out)
+    nameGenerator.parse()
